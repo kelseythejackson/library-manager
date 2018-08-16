4513b39796f8c002396878c7310b1a86eb84549a
Kelsey Jackson <kelseythej@gmail.com>
2018-08-15 20:49:47 -0500

    Finish book listing page

--- routes/books.js
+++ routes/books.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Book = require('../models').Book;
 var Loan = require('../models').Loan;
+const Op = require('sequelize').Op;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -14,7 +15,15 @@ router.get('/', function(req, res, next) {
 
 router.get('/overdue', function(req, res, next) {
     Book.findAll({
-        include: Loan
+        include: [{
+            model: Loan,
+            where: {
+                returned_on: null,
+                return_by: {
+                    $lt: new Date()
+                }
+            }
+        }]
     }).then(function(books) {
         console.log(books[0]);
         res.render('books/index', { books, title: "Overdue Books" });
@@ -23,10 +32,37 @@ router.get('/overdue', function(req, res, next) {
 });
 router.get('/checked-out', function(req, res, next) {
     Book.findAll({
-        include: Loan
+        include: [{
+            model: Loan,
+            where: {
+                returned_on: null,
+
+            }
+        }]
     }).then(function(books) {
         console.log(books[0]);
         res.render('books/index', { books, title: "Checked Out Books" });
+    });
+
+});
+
+router.get('/new', function(req, res, next) {
+    res.render('books/new/index');
+});
+
+router.get('/:id', function(req, res, next){
+    Book.findOne({
+        include: [{
+           model: Loan
+        }],
+        where: {
+            id: {
+                [Op.eq]: req.params.id
+            }
+        }
+    }).then(function(book){
+        console.log(book.Loans);
+        res.render('books/detail', {book: book});
     });
 
 });
