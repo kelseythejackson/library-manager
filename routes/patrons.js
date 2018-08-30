a76c7ebb5fce22b3d134f062093aeb69996ef6a7
Kelsey Jackson <kelseythej@gmail.com>
2018-08-29 22:16:17 -0500

    build patron templates

--- routes/patrons.js
+++ routes/patrons.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const Book = require('../models').Book;
+const Loan = require('../models').Loan;
+const Patron = require('../models').Patron;
+const Op = require('sequelize').Op;
+const moment = require('moment');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('patrons/index');
+    Patron.findAll().then(function(patrons){
+        res.render('patrons/index', { patrons });
+    })
+});
+
+router.get('/new', function(req, res, next) {
+    res.render('patrons/new/index');
+});
+
+router.get('/:id', function(req, res, next) {
+    res.render('patrons/detail');
 });
 
 module.exports = router;
